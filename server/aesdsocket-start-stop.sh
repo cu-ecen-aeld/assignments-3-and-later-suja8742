d0838a67c8c717c68ddb39dffcc89d9637adaec8
suja8742 <suja8742@colorado.edu>
2023-02-26 17:13:05 -0700

    assignment-5-part-2

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,12 +3,10 @@
 case "$1" in
     start)
         echo "Starting aesdsocket"
-        /usr/bin/aesdchar_load
         start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket
         ;;
     stop)
         echo "Stopping aesdsocket"
-        /usr/bin/aesdchar_unload
         start-stop-daemon -K -n aesdsocket
         ;;
     *)
